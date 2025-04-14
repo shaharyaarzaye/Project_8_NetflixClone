a0947b5a14c3f5a816d8755587343aeeae26834c
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2025-04-14 10:15:21 +0000

    build login and authScreen page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {Route , Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import SignUpPage from '../pages/SignUpPage'
-import HomePage from '../pages/HomePage'
+import HomePage from '../pages/home/HomePage'
 
 function App() {
 
